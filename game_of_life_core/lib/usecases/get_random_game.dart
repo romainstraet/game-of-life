c18cecc198a0e203bcdd96af7571bbb39a1fb883
Romain <romainstraet@gmail.com>
2020-07-25 21:58:15 +0200

    Core : GetRandomGame - Add 3rd test and make it pass

--- game_of_life_core/lib/usecases/get_random_game.dart
+++ game_of_life_core/lib/usecases/get_random_game.dart
@@ -3,9 +3,13 @@ import 'dart:math';
 import 'package:game_of_life_core/entites/cell_state.dart';
 
 class GetRandomGame {
+  GetRandomGame([this._rows = 30, this._cols = 30]);
+  final int _rows;
+  final int _cols;
+
   List<List<CellState>> call() {
     final List<List<CellState>> game = <List<CellState>>[];
-    for (int row = 0; row < 30; row++) {
+    for (int row = 0; row < _rows; row++) {
       game.add(_generateNewRow());
     }
     return game;
@@ -13,7 +17,7 @@ class GetRandomGame {
 
   List<CellState> _generateNewRow() {
     final List<CellState> row = <CellState>[];
-    for (int col = 0; col < 30; col++) {
+    for (int col = 0; col < _cols; col++) {
       row.add(_getRandomCellState());
     }
     return row;
--- game_of_life_core/test/usecases/get_random_game_test.dart
+++ game_of_life_core/test/usecases/get_random_game_test.dart
@@ -17,5 +17,22 @@ void main() {
       expect(numOfAliveCellsInFirstRow, lessThan(30));
       expect(numOfDeadCellsInLastRow, lessThan(30));
     });
+    test('Should return a customatized grid game', () {
+      final List<List<CellState>> game1 = GetRandomGame(20, 20).call();
+      expect(game1.length, equals(20));
+      expect(game1[0].length, equals(20));
+
+      final List<List<CellState>> game2 = GetRandomGame(10, 10).call();
+      expect(game2.length, equals(10));
+      expect(game2[0].length, equals(10));
+
+      final List<List<CellState>> game3 = GetRandomGame(10, 20).call();
+      expect(game3.length, equals(10));
+      expect(game3[0].length, equals(20));
+
+      final List<List<CellState>> game4 = GetRandomGame(20, 10).call();
+      expect(game4.length, equals(20));
+      expect(game4[0].length, equals(10));
+    });
   });
 }
