e3db42246c9d52414cbbfe735988765540ffe9f1
Romain <romainstraet@gmail.com>
2020-07-25 21:33:59 +0200

    Core : GetRandomGame - Add Second Test

--- game_of_life_core/test/usecases/get_random_game_test.dart
+++ game_of_life_core/test/usecases/get_random_game_test.dart
@@ -10,5 +10,12 @@ void main() {
       expect(game[15].length, equals(30));
       expect(game[29].length, equals(30));
     });
+    test('Should return a 30x30 grid game with random state', () {
+      final List<List<CellState>> game = GetRandomGame().call();
+      final int numOfAliveCellsInFirstRow = game[0].where((CellState el) => el == CellState.alive).length;
+      final int numOfDeadCellsInLastRow = game[29].where((CellState el) => el == CellState.dead).length;
+      expect(numOfAliveCellsInFirstRow, lessThan(30));
+      expect(numOfDeadCellsInLastRow, lessThan(30));
+    });
   });
 }
