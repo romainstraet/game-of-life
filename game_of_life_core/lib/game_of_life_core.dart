eb8b88c8ae5127c0cfab11df6d407b02c4514e49
Romain <romainstraet@gmail.com>
2020-07-25 12:15:41 +0200

    Core : Create interface and export it in library

--- game_of_life_core/lib/game_of_life_core.dart
+++ game_of_life_core/lib/game_of_life_core.dart
@@ -1,5 +1,7 @@
 library game_of_life_core;
 
+export 'interfaces/game_repository.dart';
+
 export 'usecases/get_all_games.dart';
 export 'usecases/get_next_game_state.dart';
 export 'usecases/get_random_game.dart';
