6ba956ea60b61e4d96b9c3723a541433f3d0208b
Romain <romainstraet@gmail.com>
2020-07-25 21:26:21 +0200

    Core : GetRandomGame - 1st test pass

--- game_of_life_core/lib/usecases/get_random_game.dart
+++ game_of_life_core/lib/usecases/get_random_game.dart
@@ -1,3 +1,15 @@
+import 'package:game_of_life_core/entites/cell_state.dart';
+
 class GetRandomGame {
-  call() {}
+  List<List<CellState>> call() {
+    final List<List<CellState>> game = <List<CellState>>[];
+    for (int row = 0; row < 30; row++) {
+      final List<CellState> row = <CellState>[];
+      for (int col = 0; col < 30; col++) {
+        row.add(CellState.alive);
+      }
+      game.add(row);
+    }
+    return game;
+  }
 }
--- game_of_life_core/test/usecases/get_random_game_test.dart
+++ game_of_life_core/test/usecases/get_random_game_test.dart
@@ -3,12 +3,12 @@ import 'package:test/test.dart';
 
 void main() {
   group('Testing GetRandomCall().call()', () {
-    test('Should return a 30x30 grid game with random state', () {
-      List<List<CellState>> game = GetRandomGame().call();
+    test('Should return a 30x30 grid game', () {
+      final List<List<CellState>> game = GetRandomGame().call();
       expect(game.length, equals(30));
-      expect(game[0], equals(30));
-      expect(game[15], equals(30));
-      expect(game[29], equals(30));
+      expect(game[0].length, equals(30));
+      expect(game[15].length, equals(30));
+      expect(game[29].length, equals(30));
     });
   });
 }
