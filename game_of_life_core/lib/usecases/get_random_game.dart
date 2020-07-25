afa70bb1bf6420263fee35a5991154779b7b670e
Romain <romainstraet@gmail.com>
2020-07-25 21:42:54 +0200

    Core : GetRandomGame - Make test pass

--- game_of_life_core/lib/usecases/get_random_game.dart
+++ game_of_life_core/lib/usecases/get_random_game.dart
@@ -1,15 +1,25 @@
+import 'dart:math';
+
 import 'package:game_of_life_core/entites/cell_state.dart';
 
 class GetRandomGame {
   List<List<CellState>> call() {
     final List<List<CellState>> game = <List<CellState>>[];
     for (int row = 0; row < 30; row++) {
-      final List<CellState> row = <CellState>[];
-      for (int col = 0; col < 30; col++) {
-        row.add(CellState.alive);
-      }
-      game.add(row);
+      game.add(_generateNewRow());
     }
     return game;
+  }
+
+  List<CellState> _generateNewRow() {
+    final List<CellState> row = <CellState>[];
+    for (int col = 0; col < 30; col++) {
+      row.add(_getRandomCellState());
+    }
+    return row;
+  }
+
+  CellState _getRandomCellState() {
+    return Random().nextInt(4) < 3 ? CellState.dead : CellState.alive;
   }
 }
