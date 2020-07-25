bbcfe1b24729fd77f44a7385cf4226ba1722366c
Romain <romainstraet@gmail.com>
2020-07-25 21:04:37 +0200

    Core : GetRandomGame - Write 1st test

---
+++ game_of_life_core/test/usecases/get_random_game_test.dart
@@ -0,0 +1,16 @@
+import 'dart:svg';
+
+import 'package:game_of_life_core/game_of_life_core.dart';
+import 'package:test/test.dart';
+
+main() {
+  group('Testing GetRandomCall().call()', () {
+    test('Should return a 30x30 grid game with random state', () {
+      List<List<CellState>> game = GetRandomGame().call();
+      expect(game.length, equals(30));
+      expect(game[0], equals(30));
+      expect(game[15], equals(30));
+      expect(game[29], equals(30));
+    });
+  });
+}
