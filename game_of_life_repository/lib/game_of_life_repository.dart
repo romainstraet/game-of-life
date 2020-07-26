45a58686bb6f1b053f1b6f06c3d73a55593dbbba
Romain <romainstraet@gmail.com>
2020-07-26 16:00:20 +0200

    Repo : Implements Core Repo Interface

--- game_of_life_repository/lib/game_of_life_repository.dart
+++ game_of_life_repository/lib/game_of_life_repository.dart
@@ -1,1 +1,3 @@
 library game_of_life_repository;
+
+export 'src/game_repository_in_memory.dart';
--- game_of_life_repository/lib/src/game_repository_in_memory.dart
+++ game_of_life_repository/lib/src/game_repository_in_memory.dart
@@ -2,8 +2,17 @@ import 'package:game_of_life_core/game_of_life_core.dart';
 
 class GameRepositoryInMemory implements GameRepository {
   @override
-  Future<List<Map<String, List<List<CellState>>>>> getAllGames() {
-    // TODO: implement getAllGames
-    throw UnimplementedError();
+  Future<List<Map<String, List<List<CellState>>>>> getAllGames() async {
+    final Map<String, List<List<CellState>>> game1 = <String, List<List<CellState>>>{
+      'Random game 1': GetRandomGame().call(),
+    };
+    final Map<String, List<List<CellState>>> game2 = <String, List<List<CellState>>>{
+      'Random game 2': GetRandomGame().call(),
+    };
+    final Map<String, List<List<CellState>>> game3 = <String, List<List<CellState>>>{
+      'Random game 3': GetRandomGame().call(),
+    };
+    // ignore: always_specify_types
+    return Future.value([game1, game2, game3]);
   }
 }
