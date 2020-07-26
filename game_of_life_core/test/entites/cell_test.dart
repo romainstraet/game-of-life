0a1574b6104c95497585b371e0467eca3f305968
Romain <romainstraet@gmail.com>
2020-07-26 12:04:13 +0200

    Core : Cell - write 3rd and 4th test (pass) and 5th test

--- game_of_life_core/test/entites/cell_test.dart
+++ game_of_life_core/test/entites/cell_test.dart
@@ -4,13 +4,27 @@ import 'package:test/test.dart';
 
 void main() {
   group('Testing Cell(state).getNextState()', () {
-    test('When cell is alive and have 0 or 1 neighbour, Should return dead state', () {
+    test('When cell is alive and have 0 or 1 alive neighbour, Should return dead state', () {
       expect(Cell(CellState.alive).getNextState(0), CellState.dead);
       expect(Cell(CellState.alive).getNextState(1), CellState.dead);
     });
-    test('When cell is alive and have 2 or 3 neighbours, Should return alive state', () {
+    test('When cell is alive and have 2 or 3 alive neighbours, Should return alive state', () {
       expect(Cell(CellState.alive).getNextState(2), CellState.alive);
       expect(Cell(CellState.alive).getNextState(3), CellState.alive);
+    });
+    test('When cell is alive and have more than 3 alive neighbours, Should return alive state', () {
+      expect(Cell(CellState.alive).getNextState(4), CellState.dead);
+      expect(Cell(CellState.alive).getNextState(5), CellState.dead);
+      expect(Cell(CellState.alive).getNextState(6), CellState.dead);
+      expect(Cell(CellState.alive).getNextState(7), CellState.dead);
+      expect(Cell(CellState.alive).getNextState(8), CellState.dead);
+    });
+    test('When cell is dead and have 0 or 1 alive neighbour, Should return dead state', () {
+      expect(Cell(CellState.dead).getNextState(0), CellState.dead);
+      expect(Cell(CellState.dead).getNextState(1), CellState.dead);
+    });
+    test('When cell is dead and have 2 alive neighbours, Should return dead state', () {
+      expect(Cell(CellState.dead).getNextState(2), CellState.dead);
     });
   });
 }
