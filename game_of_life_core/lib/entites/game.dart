5dbb48ae993bd0a9ddb2550a94cea6947dbbf4cb
Romain <romainstraet@gmail.com>
2020-07-26 14:47:50 +0200

    Core : Game - Pass test 4

--- game_of_life_core/lib/entites/game.dart
+++ game_of_life_core/lib/entites/game.dart
@@ -5,11 +5,15 @@ class Game {
   final List<List<CellState>> _state;
 
   int countAliveNeighboursOfCell(int row, int col) {
+    final CellState currentCell = _state[row][col];
     final List<CellState> firstRow = _state[0];
     final List<CellState> lastRow = _state[_state.length - 1];
     final int numOfAliveCellsOnTopRow = firstRow.where((CellState el) => el == CellState.alive).length;
     final int numOfAliveCellsOnBottomRow = lastRow.where((CellState el) => el == CellState.alive).length;
-
-    return numOfAliveCellsOnTopRow + numOfAliveCellsOnBottomRow;
+    int numOfAliveCells = numOfAliveCellsOnTopRow + numOfAliveCellsOnBottomRow;
+    if (currentCell == CellState.alive) {
+      numOfAliveCells -= 1;
+    }
+    return numOfAliveCells;
   }
 }
