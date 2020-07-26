db831d5f067f37364cf9add03f891f32d3767c26
Romain <romainstraet@gmail.com>
2020-07-26 11:55:26 +0200

    Core : Cell - Write 1st test

---
+++ game_of_life_core/test/entites/cell_test.dart
@@ -0,0 +1,11 @@
+import 'package:game_of_life_core/game_of_life_core.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Testing Cell(state).getNextState()', () {
+    test('When cell is alive and have 0 or 1 neighbour, Should return dead state', () {
+      expect(Cell(CellState.alive).getNextState(0), CellState.dead);
+      expect(Cell(CellState.alive).getNextState(1), CellState.dead);
+    });
+  });
+}
