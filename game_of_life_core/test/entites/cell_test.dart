f8001126c61c0978153d32f83ecd05c85616b3d5
Romain <romainstraet@gmail.com>
2020-07-26 11:58:37 +0200

    Core : Cell - Write 2nd test

--- game_of_life_core/test/entites/cell_test.dart
+++ game_of_life_core/test/entites/cell_test.dart
@@ -8,5 +8,9 @@ void main() {
       expect(Cell(CellState.alive).getNextState(0), CellState.dead);
       expect(Cell(CellState.alive).getNextState(1), CellState.dead);
     });
+    test('When cell is alive and have 2 or 3 neighbours, Should return alive state', () {
+      expect(Cell(CellState.alive).getNextState(2), CellState.alive);
+      expect(Cell(CellState.alive).getNextState(3), CellState.alive);
+    });
   });
 }
