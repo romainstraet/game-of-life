f990ee3e1908dcd6043a266d1b87897814012010
Romain <romainstraet@gmail.com>
2020-07-26 15:10:32 +0200

    Core : Game - Pass test 5

--- game_of_life_core/lib/entites/game.dart
+++ game_of_life_core/lib/entites/game.dart
@@ -6,18 +6,44 @@ class Game {
 
   int countAliveNeighboursOfCell(int row, int col) {
     final CellState currentCell = _state[row][col];
-    // final List<CellState> firstRow = _state[0];
-    // final List<CellState> lastRow = _state[_state.length - 1];
+    final List<List<CellState>> reducedGame = _reduceGameToNeighboursOfCell(row, col);
     int numOfAliveCells = 0;
-    for (final List<CellState> row in _state) {
+    for (final List<CellState> row in reducedGame) {
       numOfAliveCells += row.where((CellState el) => el == CellState.alive).length;
     }
-    // final int numOfAliveCellsOnTopRow = firstRow.where((CellState el) => el == CellState.alive).length;
-    // final int numOfAliveCellsOnBottomRow = lastRow.where((CellState el) => el == CellState.alive).length;
-    // int numOfAliveCells = numOfAliveCellsOnTopRow + numOfAliveCellsOnBottomRow;
     if (currentCell == CellState.alive) {
       numOfAliveCells -= 1;
     }
     return numOfAliveCells;
+  }
+
+  List<List<CellState>> _reduceGameToNeighboursOfCell(int row, int col) {
+    final int startRowIndexIncl = _isFirstRow(row) ? row : row - 1;
+    final int endRowIndexExcl = _isLastRow(row) ? row + 1 : row + 2;
+    final List<List<CellState>> reducedGameState = <List<CellState>>[];
+    final List<List<CellState>> reducedRows = _state.getRange(startRowIndexIncl, endRowIndexExcl).toList();
+    for (final List<CellState> rowList in reducedRows) {
+      final int startColIndexIncl = _isFirstCol(col) ? col : col - 1;
+      final int endColIndexIncl = _isLastCol(col, rowList.length) ? col + 1 : col + 2;
+      final List<CellState> reducedRow = rowList.getRange(startColIndexIncl, endColIndexIncl).toList();
+      reducedGameState.add(reducedRow);
+    }
+    return reducedGameState;
+  }
+
+  bool _isFirstRow(int row) {
+    return row == 0;
+  }
+
+  bool _isLastRow(int row) {
+    return row + 1 == _state.length;
+  }
+
+  bool _isFirstCol(int col) {
+    return col == 0;
+  }
+
+  bool _isLastCol(int col, int colLength) {
+    return col + 1 == colLength;
   }
 }
