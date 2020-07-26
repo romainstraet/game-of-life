cd262b307511dfb6d68dd0b6b4e584ee15d03eab
Romain <romainstraet@gmail.com>
2020-07-26 14:53:25 +0200

    Core : Game - Refactoring before passing test 5

--- game_of_life_core/lib/entites/game.dart
+++ game_of_life_core/lib/entites/game.dart
@@ -6,11 +6,15 @@ class Game {
 
   int countAliveNeighboursOfCell(int row, int col) {
     final CellState currentCell = _state[row][col];
-    final List<CellState> firstRow = _state[0];
-    final List<CellState> lastRow = _state[_state.length - 1];
-    final int numOfAliveCellsOnTopRow = firstRow.where((CellState el) => el == CellState.alive).length;
-    final int numOfAliveCellsOnBottomRow = lastRow.where((CellState el) => el == CellState.alive).length;
-    int numOfAliveCells = numOfAliveCellsOnTopRow + numOfAliveCellsOnBottomRow;
+    // final List<CellState> firstRow = _state[0];
+    // final List<CellState> lastRow = _state[_state.length - 1];
+    int numOfAliveCells = 0;
+    for (final List<CellState> row in _state) {
+      numOfAliveCells += row.where((CellState el) => el == CellState.alive).length;
+    }
+    // final int numOfAliveCellsOnTopRow = firstRow.where((CellState el) => el == CellState.alive).length;
+    // final int numOfAliveCellsOnBottomRow = lastRow.where((CellState el) => el == CellState.alive).length;
+    // int numOfAliveCells = numOfAliveCellsOnTopRow + numOfAliveCellsOnBottomRow;
     if (currentCell == CellState.alive) {
       numOfAliveCells -= 1;
     }
