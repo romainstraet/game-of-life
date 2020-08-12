41346cacb78bae212b4762b1d41a279df2e765b6
Romain <romainstraet@gmail.com>
2020-08-12 15:58:10 +0200

    Add runApp

--- game_of_life_mobile_app/lib/main.dart
+++ game_of_life_mobile_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:game_of_life_core/game_of_life_core.dart';
 import 'package:game_of_life_repository/game_of_life_repository.dart';
 
-//import 'game_view.dart';
+import 'game_view.dart';
 import 'game_view_model.dart';
 
 void main() {
@@ -10,5 +10,5 @@ void main() {
   final GameOfLifeCoreUsecases gameOfLifeCoreUsecases = GameOfLifeCoreUsecases(gameRepositoryInMemory);
   final GameViewModel gameViewModel = GameViewModel(gameOfLifeCoreUsecases);
   gameViewModel.onInit();
-  //runApp(GameView(gameViewModel));
+  runApp(GameView(gameViewModel));
 }
